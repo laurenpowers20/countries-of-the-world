01b1cd786222b8627b4f0d49c2f16cc255f63c29
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-10-31 09:54:44 -0400

    add dependencies and edits to deploy in mongo and railway

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+const url = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/countries_db";
+
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/countries_db").catch((err) => {
+mongoose.connect(url).catch((err) => {
   console.log(`Error connection go MongoDB: ${err.message}`);
 });
 
